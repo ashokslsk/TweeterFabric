04ef4172d558b09f4d7b9d48af440f93bbffe076
Ashok Kumar <ashokslsk@gmail.com>
2015-08-23 09:45:47 +0530

    Integrated twitter fabric sdk

--- app/src/main/java/com/ashokslsk/twittersearch/MainActivity.java
+++ app/src/main/java/com/ashokslsk/twittersearch/MainActivity.java
@@ -4,12 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "RRRpRCOXJXO35xUs4UEDIwFKl";
+    private static final String TWITTER_SECRET = "U4GzJyY7Cpzlj52IhEX2VLdIB0RKPLu1PwTBsm2wAbk1BagkCn";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
     }
 
